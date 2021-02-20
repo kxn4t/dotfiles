be2c9bb87bf442e36b00de3c03692a01acb255dc
kxn4t <kxxn4t@gmail.com>
2021-02-20 20:47:49 +0900

    add git setup

---
+++ setup-git.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+git config --global pull.ff only
+git config --global color.ui true
+git config --global core.editor vim
+git config --global core.quotepath false
+
+# set name & email
+read -p 'Please enter your user name: ' gitname
+git config --global user.name $gitname
+read -p 'Please enter your email: ' gitemail
+git config --global user.email $gitemail
+
+echo '==== Complete git configuration ======='
+git config -l | cat
