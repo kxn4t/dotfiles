0541f53a3a69f9cd0a03494bd9448982b2225a4f
kxn4t <kxxn4t@gmail.com>
2021-02-20 22:31:43 +0900

    add *rc

---
+++ setup-rc.sh
@@ -0,0 +1,12 @@
+#!/usr/local/bin/zsh
+
+# set vim colorscheme
+mkdir ~/.vim/colors
+mkdir ./iceberg
+curl -L https://github.com/cocopon/iceberg.vim/archive/1.0.tar.gz | tar zx -C iceberg --strip-components 1
+mv iceberg/colors/iceberg.vim ~/.vim/colors
+rm -rf iceberg
+
+# create a symbolic link
+ln -snf "$(cd $(dirname $0) && pwd)/.zshrc" ~/
+ln -snf "$(cd $(dirname $0) && pwd)/.vimrc" ~/
